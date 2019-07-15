01e7ffab94fb087504ce7b1b94f5d58a9f6772b6
tommycoleman87 <tommiilee@gmail.com>
2019-07-15 18:10:43 -0400

    added a single event handler to handle yards to go and ball on functions

--- src/App.js
+++ src/App.js
@@ -13,41 +13,48 @@ function App() {
   const [ballOn, setBallOn] = useState(0);
 
   const scoreHandler = (team, typeOfScore) => {
-    if(team === 'Lions') {
-      switch(typeOfScore === 'touchDown') {
-        case true: 
-        return setHomeScore(homeScore + 7);
-        case false: 
-        return setHomeScore(homeScore + 3);
-        default: 
-        return homeScore;
+    if (team === "Lions") {
+      switch (typeOfScore === "touchDown") {
+        case true:
+          return setHomeScore(homeScore + 7);
+        case false:
+          return setHomeScore(homeScore + 3);
+        default:
+          return homeScore;
       }
-    } else if(team === 'Tigers') {
-      switch(typeOfScore === 'touchDown') {
-        case true: 
-        return setAwayScore(awayScore + 7);
-        case false: 
-        return setAwayScore(awayScore + 3);
-        default: 
-        return awayScore;
+    } else if (team === "Tigers") {
+      switch (typeOfScore === "touchDown") {
+        case true:
+          return setAwayScore(awayScore + 7);
+        case false:
+          return setAwayScore(awayScore + 3);
+        default:
+          return awayScore;
       }
     } else {
       return;
     }
-  }
+  };
 
-  const yardsToGoHandler = (event) => {
-    console.log(event.target.value.length)
-    if(event.target.value <= 10 && event.target.value.length > 0) {
-    return setYards(event.target.value)
-  }
-  }
+ 
 
-  const ballOnHandler = (event) => {
-    console.log(event.target.value.length)
-    if(event.target.value <= 100 && event.target.value.length > 0) {
-    return setBallOn(event.target.value)
-  }
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(event.target)
+    if(event.target.firstChild.classList.contains('yardsToGo')) {
+      if(event.target.firstChild.value <= 10 && event.target.firstChild.value.length > 0) {
+        return setYards(event.target.firstChild.value) }
+        else {
+          alert('Please enter in a number between 1 and 10');
+        }
+    } else {
+      if(event.target.firstChild.value <= 100 && event.target.firstChild.value.length > 0) {
+      return setBallOn(event.target.firstChild.value);
+      } else {
+        alert('Please enter in a number between 1 and 100')
+      }
+    }
+    
   }
 
   return (
@@ -67,29 +74,72 @@ function App() {
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
-        <BottomRow quarter={quarter} yardsToGo={yardsToGo} down={down} ballOn={ballOn}/>
+        <BottomRow
+          quarter={quarter}
+          yardsToGo={yardsToGo}
+          down={down}
+          ballOn={ballOn}
+        />
       </section>
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => scoreHandler('Lions', 'touchDown')}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={() => scoreHandler('Lions', 'fieldGoal')}>Home Field Goal</button>
+          <button
+            className="homeButtons__touchdown"
+            onClick={() => scoreHandler("Lions", "touchDown")}
+          >
+            Home Touchdown
+          </button>
+          <button
+            className="homeButtons__fieldGoal"
+            onClick={() => scoreHandler("Lions", "fieldGoal")}
+          >
+            Home Field Goal
+          </button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => scoreHandler('Tigers', 'touchDown')}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => scoreHandler('Tigers', 'fieldGoal')}>Away Field Goal</button>
+          <button
+            className="awayButtons__touchdown"
+            onClick={() => scoreHandler("Tigers", "touchDown")}
+          >
+            Away Touchdown
+          </button>
+          <button
+            className="awayButtons__fieldGoal"
+            onClick={() => scoreHandler("Tigers", "fieldGoal")}
+          >
+            Away Field Goal
+          </button>
         </div>
         <div className="bottomButtons">
-          <button className="downButton" onClick={() => setDown(down < 4 ? down + 1 : down - 3)}>Change Downs</button>
-          <button className="quarterButton" onClick={() => setQuarter(quarter < 4 ? quarter + 1 : quarter - 3)}>Change Quarter</button>
+          <button
+            className="downButton"
+            onClick={() => setDown(down < 4 ? down + 1 : down - 3)}
+          >
+            Change Downs
+          </button>
+          <button
+            className="quarterButton"
+            onClick={() => setQuarter(quarter < 4 ? quarter + 1 : quarter - 3)}
+          >
+            Change Quarter
+          </button>
         </div>
         <div className="bottomInputs">
           <h3>Change Yards to Go</h3>
-          <input className="yardsToGo" type="number" onChange={(event) => yardsToGoHandler(event)}/>
+          <form onSubmit={(event) => handleSubmit(event)}>
+          <input
+            className="yardsToGo"
+            type="number"
+          />
+          </form>
           <h3>Change Ball On</h3>
-          <input className="ballOnButton" onChange={(event) => ballOnHandler(event)} />
+          <form onSubmit={(event) => handleSubmit(event)}>
+          <input
+            className="ballOnButton"
+          />
+          </form>
         </div>
-        
       </section>
     </div>
   );
